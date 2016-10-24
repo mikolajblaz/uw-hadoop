84f280f15a4aa5a57c44c5031880df717195bb38
Mikołaj Błaż <mikolajblaz@gmail.com>
2016-10-24 15:29:47 +0200

    Read from directories

--- src/Similarity.java
+++ src/Similarity.java
@@ -1,6 +1,8 @@
 import javafx.util.Pair;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
 import java.util.*;
@@ -11,19 +13,30 @@ import java.util.*;
 
 public class Similarity {
     public static void main(String[] args) throws IOException {
-        String[] files = args;
+        Path inputDir = new Path(args[0]);
 
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(conf);
+
+        FileStatus[] inputFiles = fs.listStatus(inputDir);
+        String[] files = new String[inputFiles.length];
+        String[] filenames = new String[inputFiles.length];
+        Path path;
+        for (int i = 0; i < inputFiles.length; i++) {
+            path = inputFiles[i].getPath();
+            files[i] = path.toString();
+            filenames[i] = path.getName();
+            System.out.println(files[i]);
+        }
 
         LSH lsh = new LSH(files, fs);
         List<Pair<Integer, Integer>> pairs = lsh.doLSH(true);
 
         System.out.println("Candidate pairs:");
         for (Pair<Integer, Integer> p : pairs) {
-            System.out.println("(" + files[p.getKey()] + ", " + files[p.getValue()] + ")");
+            System.out.println("(" + filenames[p.getKey()] + ", " + filenames[p.getValue()] + ")");
         }
-        System.out.println("\nTotal: " + Integer.toString(files.length));
+        System.out.println("\nTotal: " + Integer.toString(pairs.size()));
     }
 
 
